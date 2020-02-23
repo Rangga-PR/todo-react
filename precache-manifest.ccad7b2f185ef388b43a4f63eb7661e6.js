6ce18b5796bf23fe279d74c5f59810a3c47de900
Rangga Putra Raharja <rangga-pr@pop-os.localdomain>
2020-02-23 19:22:32 +0700

    Updates

---
+++ precache-manifest.ccad7b2f185ef388b43a4f63eb7661e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd6574ab25f1b0df30b4290ec97a925",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "c18fb423307ebed1254b",
+    "url": "/todo-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c4e5a1b86686c6e789e0",
+    "url": "/todo-react/static/js/2.db935d30.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/todo-react/static/js/2.db935d30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c18fb423307ebed1254b",
+    "url": "/todo-react/static/js/main.3d4c1b6f.chunk.js"
+  },
+  {
+    "revision": "965f405a70b798f3267a",
+    "url": "/todo-react/static/js/runtime-main.e8052da2.js"
+  }
+]);
